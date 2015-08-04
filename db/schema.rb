4b2a9ae70c884a185d7c76839ad2ca752bd618fa
Ahmed-wael <ahmedwael2002@hotmail.com>
2015-08-04 02:08:09 +0200

    Creating devise table and connecting it with the two types of users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150730233542) do
+ActiveRecord::Schema.define(version: 20150803230837) do
 
   create_table "cloths", force: :cascade do |t|
     t.string   "size"
@@ -36,6 +36,19 @@ ActiveRecord::Schema.define(version: 20150730233542) do
     t.datetime "updated_at",    null: false
   end
 
+  create_table "members", force: :cascade do |t|
+    t.string   "f_name"
+    t.string   "l_name"
+    t.integer  "role"
+    t.text     "bio"
+    t.string   "phone"
+    t.string   "address"
+    t.string   "photo"
+    t.integer  "devise_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "organizations", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
@@ -47,16 +60,21 @@ ActiveRecord::Schema.define(version: 20150730233542) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "f_name"
-    t.string   "l_name"
-    t.integer  "role"
-    t.text     "bio"
-    t.string   "phone"
-    t.text     "address"
-    t.text     "photo"
-    t.integer  "device_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
