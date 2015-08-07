a9c0d2dbeae87d8dbab177ac41471257b5482dd3
Ahmed-wael <ahmedwael2002@hotmail.com>
2015-08-07 18:20:29 +0200

    modifing paperclip to view the members photo

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,9 @@
 class Member < ActiveRecord::Base
 	has_many :medicines , dependent: :destroy
 	has_many :cloths , dependent: :destroy
+
+		attr_accessor :photo_file_name
+
+	  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url =>   "/assets/users/:id/:style/:basename.:extension"
+  validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+   has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url =>  "/assets/users/:id/:style/:basename.:extension"
+  validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 end
