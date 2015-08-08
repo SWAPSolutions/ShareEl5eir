89a035f04c133f680770516be00b49d57d641612
Ahmed-wael <ahmedwael2002@hotmail.com>
2015-08-08 14:58:56 +0200

    The profile picture now works on the show page

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,8 +2,8 @@ class Member < ActiveRecord::Base
 	has_many :medicines , dependent: :destroy
 	has_many :cloths , dependent: :destroy
 
-		attr_accessor :photo_file_name
+	
 
-	  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url =>   "/assets/users/:id/:style/:basename.:extension"
+	  has_attached_file :photo, :styles => { :medium => "200x200>"}, :default_url =>   "/public/system/members/photos/000/:id/:style/:basename.:extension"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+    attr_accessor :photo_file_name
+    
    has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url =>  "/assets/users/:id/:style/:basename.:extension"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 end
