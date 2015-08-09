6baff141a9fff1b0bba0fe82f7577912bd04aa82
Ahmed-wael <ahmedwael2002@hotmail.com>
2015-08-09 15:43:16 +0200

    showing the photos in the organizations page

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,5 @@
 class Organization < ActiveRecord::Base
+
+	 has_attached_file :photo, :styles => { :medium => "200x200>"}, :default_url =>   "/public/system/oragnizations/photos/000/:id/:style/:basename.:extension"
+  validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 end
