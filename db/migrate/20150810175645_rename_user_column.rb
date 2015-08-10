5b6ab70136e244680a548475483516e10a2a27dd
Omar Saleh <omarsaleh0106@gmail.com>
2015-08-10 20:00:54 +0200

    fixed a typo in the migration file

--- db/migrate/20150810175645_rename_user_column.rb
+++ db/migrate/20150810175645_rename_user_column.rb
@@ -1,9 +1,11 @@
 class RenameUserColumn < ActiveRecord::Migration
   def self.up
-  	rename_column :colths , :user_id , :member_id
+  	rename_column :cloths , :user_id , :member_id
+  	rename_column :medicines , :user_id , :member_id
   end
 
   def self.down
   	rename_column :cloths , :member_id , :user_id
+  	rename_column :medicines , :member_id , :user_id
   end
 end
